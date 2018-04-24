c51145bd45ba8436cd889ff6b7952e571bde7fe4
Janus <Janus.xu@outlook.com>
2018-04-24 22:40:55 +0800

    添加注释

--- samples/core/get_started/iris_data.py
+++ samples/core/get_started/iris_data.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import tensorflow as tf
 
+#训练集
 TRAIN_URL = "http://download.tensorflow.org/data/iris_training.csv"
+
+#测试集
 TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
+# 解析csv文件列，前四个特征值，最后为所属标签
 CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth',
                     'PetalLength', 'PetalWidth', 'Species']
+
+# 标签
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
 def maybe_download():
@@ -14,11 +20,20 @@ def maybe_download():
 
     return train_path, test_path
 
+'''
+返回两个 (feature,label) 对，分别对应训练集和测试集
+'''
 def load_data(y_name='Species'):
+
     """Returns the iris dataset as (train_x, train_y), (test_x, test_y)."""
     train_path, test_path = maybe_download()
 
     train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+
+    '''
+    从DataFrame中丢弃指定表头名称的列，并返回该列
+    下方传入的y_name = Species,即标签分类,把元数据拆分为两个DataFrame：..x和..y；..x为特征数据集；..y为对应标签数据集
+    '''
     train_x, train_y = train, train.pop(y_name)
 
     test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
@@ -27,12 +42,26 @@ def load_data(y_name='Species'):
     return (train_x, train_y), (test_x, test_y)
 
 
+#
+
+'''
+训练模型时调用，用于提供训练数据
+
+train_feature 是 Python 字典，其中：每个键都是特征的名称。每个值都是包含训练集中每个样本的值的数组。
+train_label 是包含训练集中每个样本的标签值的数组。
+args.batch_size 是一个定义批次大小的整数。
+'''
 def train_input_fn(features, labels, batch_size):
     """An input function for training"""
     # Convert the inputs to a Dataset.
+
+    # 输入特征和标签转换为 tf.data.Dataset 对象，固定语法
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
     # Shuffle, repeat, and batch the examples.
+    # Shuffle：如果训练样本是随机排列的，则训练效果最好。随机化处理
+    # repeat: 训练方法通常会多次处理样本。在不使用任何参数的情况下调用 tf.data.Dataset.repeat 方法可确保 train 方法拥有无限量的训练集样本
+    # batch: 一次处理一批样本;默认批次大小设置为 100，意味着 batch 方法将组合多个包含 100 个样本的组。较小的批次大小通常会使 train 方法（有时）以牺牲准确率为代价来加快训练模型。
     dataset = dataset.shuffle(1000).repeat().batch(batch_size)
 
     # Return the dataset.
@@ -49,13 +78,16 @@ def eval_input_fn(features, labels, batch_size):
         inputs = (features, labels)
 
     # Convert the inputs to a Dataset.
+    # 1. 将测试集中的特征和标签转换为 tf.dataset 对象。
     dataset = tf.data.Dataset.from_tensor_slices(inputs)
 
     # Batch the examples
+    # 2.创建一批测试集样本。（无需随机化处理或重复使用测试集样本。）
     assert batch_size is not None, "batch_size must not be None"
     dataset = dataset.batch(batch_size)
 
     # Return the dataset.
+    # 3.将该批次的测试集样本返回 classifier.evaluate。
     return dataset
 
 
--- samples/core/get_started/premade_estimator.py
+++ samples/core/get_started/premade_estimator.py
@@ -21,41 +21,87 @@ import tensorflow as tf
 
 import iris_data
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--train_steps', default=1000, type=int,
                     help='number of training steps')
 
+
 def main(argv):
     args = parser.parse_args(argv[1:])
 
+    print(type(args.train_steps))
+
     # Fetch the data
+
+    '''
+    ---1. 导入和解析数据集---
+    '''
     (train_x, train_y), (test_x, test_y) = iris_data.load_data()
 
     # Feature columns describe how to use the input.
+    # 通过调用 tf.feature_column 模块中的函数来构建 feature_column 对象列表
+    '''
+    ---2. 描述数据---
+    
+    [_NumericColumn(key='SepalLength', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None),
+     _NumericColumn(key='SepalWidth', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None), 
+     _NumericColumn(key='PetalLength', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None), 
+     _NumericColumn(key='PetalWidth', shape=(1,), default_value=None, dtype=tf.float32, normalizer_fn=None)]
+    '''
     my_feature_columns = []
     for key in train_x.keys():
         my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 
     # Build 2 hidden layer DNN with 10, 10 units respectively.
+
+    '''
+    ---3. 选择模型类型---
+    
+    创建训练模型：选择神经网络->要指定模型类型，请实例化一个 Estimator 类。
+    此处使用预创建的 Estimator tf.estimator.DNNClassifier
+    此 Estimator 会构建一个对样本进行分类的神经网络。
+    以下为实例化DNNClassifier
+    '''
     classifier = tf.estimator.DNNClassifier(
         feature_columns=my_feature_columns,
         # Two hidden layers of 10 nodes each.
+        # hidden_units 定义神经网络内每个隐藏层中的神经元数量。（本例中第一个隐藏层中有 10 个，第二个隐藏层中有 10 个）
+        # 要更改隐藏层或神经元的数量，只需为 hidden_units 参数分配另一个列表即可。
         hidden_units=[10, 10],
         # The model must choose between 3 classes.
+        # 指定了神经网络可以预测的潜在值的数量，当前问题需要把结果分为3类
         n_classes=3)
+    # 还可以指定优化器，优化器和学习速率也非常重要
 
     # Train the Model.
+    '''
+    ---4. 训练模型---
+    
+    steps 指示 train 在完成指定的迭代次数后停止训练
+    增加 steps 会延长模型训练的时间,args.train_steps 的默认值是 1000;
+    训练的步数是一个可以调整的超参数。选择正确的步数通常需要一定的经验和实验基础。
+    
+    input_fn,在iris_data中存在。会确定提供训练数据的函数
+    '''
     classifier.train(
-        input_fn=lambda:iris_data.train_input_fn(train_x, train_y,
-                                                 args.batch_size),
+        # args.batch_size 是一个定义批次大小的整数
+        input_fn=lambda: iris_data.train_input_fn(train_x, train_y,
+                                                  args.batch_size),
         steps=args.train_steps)
 
     # Evaluate the model.
+
+    '''
+    ---5. 评估模型---
+    
+    每个 Estimator 都提供了 evaluate 方法
+    classifier.evaluate 必须从测试集（而非训练集）中获取样本
+    eval_input_fn 函数负责提供来自测试集的一批样本。
+    '''
     eval_result = classifier.evaluate(
-        input_fn=lambda:iris_data.eval_input_fn(test_x, test_y,
-                                                args.batch_size))
+        input_fn=lambda: iris_data.eval_input_fn(test_x, test_y,
+                                                 args.batch_size))
 
     print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
@@ -69,9 +115,9 @@ def main(argv):
     }
 
     predictions = classifier.predict(
-        input_fn=lambda:iris_data.eval_input_fn(predict_x,
-                                                labels=None,
-                                                batch_size=args.batch_size))
+        input_fn=lambda: iris_data.eval_input_fn(predict_x,
+                                                 labels=None,
+                                                 batch_size=args.batch_size))
 
     template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
 
